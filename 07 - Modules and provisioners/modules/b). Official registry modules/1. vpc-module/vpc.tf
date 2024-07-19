14d67f0ee9a326a44a6e0de9882703678184ebb7
chommymotola <motola1212@gmail.com>
2024-07-19 13:02:49 -0400

    Update vpc.tf

--- 07 - Modules and provisioners/modules/b). Official registry modules/1. vpc-module/vpc.tf
+++ 07 - Modules and provisioners/modules/b). Official registry modules/1. vpc-module/vpc.tf
@@ -1,12 +1,12 @@
 # Create VPC Terraform Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.11.5"
+  version = "5.58.0"
 
   name = "vpc-dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1c"]
+  azs             = ["us-east-1a", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
